68db0d1f1e32451b7b63bbcc3d8cfd9643c6506b
Kai Xiang <kaixiang@refvy.com>
2016-01-27 10:38:20 +0000

    fix validation bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,8 @@ class UsersController < ApplicationController
   
   private 
   def validate_is_user
-    if current_user.id != params[:user_id]
+    if current_user.id.to_s != params[:user_id]
+      debugger
       flash[:danger] = "You can only view who you are following"
       redirect_to root_path
     end
