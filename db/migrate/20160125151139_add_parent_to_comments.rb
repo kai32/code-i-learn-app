17bd9871e46627d7411fe679cc8d89c35e5e6fcf
Kai Xiang <kaixiang@refvy.com>
2016-01-29 13:14:42 +0000

    trying to get production to migrate

--- db/migrate/20160125151139_add_parent_to_comments.rb
+++ db/migrate/20160125151139_add_parent_to_comments.rb
@@ -1,5 +1,5 @@
 class AddParentToComments < ActiveRecord::Migration
   def change
-    add_reference :comments, :parent, references: :comments, index: true, foreign_key: true
+    add_reference :comments, :parent, references: :comments
   end
 end
