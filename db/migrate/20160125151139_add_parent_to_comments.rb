92faf5d80ee61d0279094a6c023980e1d95d1165
Kai Xiang <kaixiang@refvy.com>
2016-01-29 13:10:20 +0000

    add references to comment migration

--- db/migrate/20160125151139_add_parent_to_comments.rb
+++ db/migrate/20160125151139_add_parent_to_comments.rb
@@ -1,5 +1,5 @@
 class AddParentToComments < ActiveRecord::Migration
   def change
-    add_reference :comments, :parent, class: 'Comment', index: true, foreign_key: true
+    add_reference :comments, :parent, references: :comments, index: true, foreign_key: true
   end
 end
